ea6009aca74e9db00a9e4535ef3c14c9e00f502a
gt-yordanos <gt.yordanos@gmail.com>
2024-12-10 17:03:20 +0300

    Created and mounted a Footer Component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../Components/TitleCards/TitleCards.jsx'
+import Footer from '../../Components/Footer/Footer.jsx'
 
 const Home = () => {
   return (
@@ -32,6 +33,7 @@ const Home = () => {
         <TitleCards title={"OUpcoming"}/>
         <TitleCards title={"Top pics for you"}/>
       </div>
+      <Footer/>
     </div>
   )
 }
