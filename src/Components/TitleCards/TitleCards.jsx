07e785bd265925f1f41f69b30583cf32f3dbddde
gt-yordanos <gt.yordanos@gmail.com>
2024-12-10 07:17:41 +0300

    Created a navbar and mounted in to the home

---
+++ src/Components/TitleCards/TitleCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './TitleCards.css'
+const TitleCards = () => {
+  return (
+    <div className='titletards'>
+      
+    </div>
+  )
+}
+
+export default TitleCards
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../Components/Navbar/Navbar.jsx'
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
