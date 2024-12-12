89c0dc2add0acb7d7fb55acada0296530ed6f356
gt-yordanos <gt.yordanos@gmail.com>
2024-12-12 10:21:58 +0300

    Added and mounted the footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Home from './Pages/Home/Home.jsx'
+import Login from './Pages/Login/Login.jsx'
+import { Routes, Route } from 'react-router-dom'
+
 const App = () => {
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+
+      </Routes>
     </div>
   )
 }
